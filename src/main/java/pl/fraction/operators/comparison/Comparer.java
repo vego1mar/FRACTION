0188a26676bd1b60db1194243d3ec5e355a7ef4e
Paweł Siciński <koryfeusz.ksiezyca@gmail.com>
2018-04-20 13:42:49 +0200

    Adding of integer fractions.

--- src/main/java/pl/fraction/operators/comparison/Comparer.java
+++ src/main/java/pl/fraction/operators/comparison/Comparer.java
@@ -12,22 +12,12 @@ public class Comparer {
     // TODO: include sign of the fraction
     // TODO: use shortened version for comparison in the overloaded function method
     public static boolean equality(@NotNull Fraction fraction1, @NotNull Fraction fraction2) {
-        char[] numerator1 = fraction1.getNumerator().toCharArray();
-        char[] numerator2 = fraction2.getNumerator().toCharArray();
-        char[] denominator1 = fraction1.getDenominator().toCharArray();
-        char[] denominator2 = fraction2.getDenominator().toCharArray();
-
-        if ((numerator1.length != numerator2.length) || (denominator1.length != denominator2.length)) {
+        if ((fraction1.getNumerator().length() != fraction2.getNumerator().length()) ||
+            (fraction1.getDenominator().length() != fraction2.getDenominator().length())) {
             return false;
         }
 
-        for (int i = 0; i < fraction1.getNumerator().toCharArray().length; i++) {
-            if ((numerator1[i] != numerator2[i]) || (denominator1[i] != denominator2[i])) {
-                return false;
-            }
-        }
-
-        return true;
+        return fraction1.getNumerator().equals(fraction2.getNumerator()) && fraction1.getDenominator().equals(fraction2.getDenominator());
     }
 
 }
